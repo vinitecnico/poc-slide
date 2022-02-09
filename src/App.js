f9467bd3c5fef4bd9aa305d39db230193a5d5b2f
vinitecnico <vinitecnico@gmail.com>
2022-02-08 21:27:46 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ImageSlider from "./components/imageSlider";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [query, setQuery] = useState(""); // declaring state variable "query" and "setQuery" method to update query state
+
+  const [images, setImages] = useState([]); // declare state variable "images" to an empty array and "setImages" method to update images state
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://pixabay.com/api/?key=6157966-0255d6fd328f89519fe0b8438&q=${query}`
+    )  // fetch to API by passing in query state
+      .then((response) => response.json())
+      .then(({ hits }) => hits.map(({ webformatURL }) => webformatURL)) // use object destructuring to grab image urls from json response
+      .then(setImages); // call setImages to update images state with image urls
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Slider</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setQuery(e.target.value)} />
+        <input type="submit" value="Search" />
+      </form>
+
+      <ImageSlider images={images} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/imageSlider.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const ImageSlider = ({ images }) => {
+  // takes in images as props
+  const [index, setIndex] = useState(0); // create state to keep track of images index, set the default index to 0
+
+  const slideRight = () => {
+    setIndex((index + 1) % images.length); // increases index by 1
+  };
+
+  const slideLeft = () => {
+    const nextIndex = index - 1;
+    if (nextIndex < 0) {
+      setIndex(images.length - 1); // returns last index of images array if index is less than 0
+    } else {
+      setIndex(nextIndex);
+    }
+  };
+
+  return (
+    images.length > 0 && (
+      <main>
+        <div>
+          <button onClick={slideLeft}>{"<"}</button>
+        </div>
+        <div>
+          <img src={images[index]} alt={index} />
+        </div>
+        <div>
+          <button onClick={slideRight}>{">"}</button>
+        </div>
+      </main>
+    )
+  );
+};
+
+export default ImageSlider;
